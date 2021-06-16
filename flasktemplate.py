2a310ba81b2f6b729a05d8f13995bc5b0a9f3efb
Jean Pereira <jeankpayales444@gmail.com>
2021-06-15 20:40:42 -0600

    changes to flask template to include another path

--- flasktemplate.py
+++ flasktemplate.py
@@ -5,12 +5,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return """<html><body>
-	<h1>Hello, world!</h1>
-	The time is """ + str(datetime.now()) + """.
-	</body></html>"""
+    return HELLO_HTML
+
+HELLO_HTML = """
+    <html><body>
+        <h1>Hello, world!</h1>
+        Click <a href="/time">here</a> for the time.
+    </body></html>
+    """
+    
+@app.route("/time")
+def time():
+    return f" {TIME_HTML} {datetime.now()}"
+
+TIME_HTML = """
+    <html><body>
+        The Date and Time are
+    </body></html>
+    """
 
 if __name__ == "__main__":
-	# Launch the Flask dev server
-	app.run(host="localhost", debug=True)
+    # Launch the Flask dev server
+    app.run(host="localhost", debug=True)
+
+
 
