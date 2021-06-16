1580d4c67439a55316bf52a1e9529e34f83b5d38
Jean Pereira <jeankpayales444@gmail.com>
2021-06-15 22:28:28 -0600

    adding a input string for the URL

--- flasktemplate.py
+++ flasktemplate.py
@@ -1,19 +1,22 @@
-from flask import Flask
+# In the URL after: http/s://<server>:port/path type ?name=<name_string> if not added it will default to World
+
+from flask import Flask, request
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return HELLO_HTML
+    name = request.args.get("name", "World")
+    return HELLO_HTML.format(name)
 
 HELLO_HTML = """
     <html><body>
-        <h1>Hello, world!</h1>
+        <h1>Hello {0}!</h1>
         Click <a href="/time">here</a> for the time.
     </body></html>
     """
-    
+
 @app.route("/time")
 def time():
     return f" {TIME_HTML} {datetime.now()}"
